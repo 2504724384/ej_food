92b21d536626bf1b4036a1c9ae386749a3794035
luq_spring <2504724384@qq.com>
2019-10-21 22:30:48 +0800

    颐记美食

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-10-21 21:31:48
+ * @LastEditTime: 2019-10-21 22:12:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /d:\briup\8vue\ej_food\src\App.vue
+ -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button class="primary">el-button</el-button>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2019-10-21 21:31:48
+ * @LastEditTime: 2019-10-21 22:11:39
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /d:\briup\8vue\ej_food\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import axios from 'axios'
+import qs from 'qs'
+
 
 Vue.config.productionTip = false
 
